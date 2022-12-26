c8295b9f2fdac2115dec831fe5d708eb224f1a12
Dhiego Dias <ddias003@gmail.com>
2022-12-26 16:50:32 -0300

    createUser controller and useCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,14 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userFoundByEmail = this.usersRepository.findByEmail(email);
+
+    if (userFoundByEmail) {
+      throw new Error("Email already taken");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
